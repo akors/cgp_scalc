84fe717cbbbc1b5c5c713a792779fc7f74023fed
Alexander Korsunsky <fat.lobyte9@gmail.com>
2010-01-22 20:42:08 +0100

    Implemted input from file
    * src/main.cpp:
     - File input is now possible by passing a filename as argument.

--- src/main.cpp
+++ src/main.cpp
@@ -22,18 +22,71 @@
 #include <cstring>
 
 extern int yyparse();
+extern FILE* yyin;
 
 #if defined(YYDEBUG)
     extern int yydebug;
 #endif
 
+const char* usage_string =
+    "Usage: scalc <options> <inputfile>\n"
+    "\t-h:\t\tDisplay help\n"
+#if defined(YYDEBUG)
+    "\t-d:\t\tDisplay parser debug information on error\n"
+#endif
+    "\n"
+    "\t<inputfile>:\tInput file for reading operations. If not specified,\n"
+    "\t\tread from stdin.\n"
+    ;
+
 int main(int argc, char** argv)
 {
+    // name of the input file, NULL if reading from stdin
+    char *infilename = NULL;
+
+    unsigned positional_count = 0;
+
+    // iterate through arguments, retrieve options
+    for (unsigned i = 1; i < argc; ++i)
+    {
+        if (!strcmp("-h", argv[i]) || !strcmp("--help", argv[i]))
+        {
+            std::cout<<usage_string;
+            return 0;
+        }
 #if defined(YYDEBUG)
-    // turn on debugging when -d option is specified
-    if (argc > 2 && !strcmp("-d", argv[2]))
-        yydebug = 1;
+        // turn on debugging when -d option is specified
+        else if (!strcmp("-d", argv[i]), !strcmp("--debug", argv[i]))
+            yydebug = 1;
 #endif
+        // retrieve positional options
+        else
+        {
+            switch(positional_count)
+            {
+                // first option is the input filename
+                case 0:
+                    infilename = argv[i];
+                    break;
+                default:
+                    std::cerr<<"Unknown option \""<<argv[i]<<"\"";
+                    return 0;
+            }
+
+            ++positional_count;
+        }
+    }
+
+    // set yyin to the input file, if specified
+    if (infilename != NULL)
+    {
+        yyin = fopen(infilename, "r");
+        if (yyin == NULL)
+        {
+            perror ("Failed to open file");
+            return 0;
+        }
+    }
 
     yyparse();
     return 0;
